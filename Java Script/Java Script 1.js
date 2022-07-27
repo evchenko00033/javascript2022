27dfcfd7b8cee7a07455bd03bf6ecf218da4b940
evchenko00033 <evchenko@ukr.net>
2022-07-27 22:21:51 +0300

    Initial commit

---
+++ Java Script/Java Script 1.js
@@ -0,0 +1,87 @@
+let a = 'hello ';
+let b = 'owu ';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(h);
+// alert(i);
+// alert(j);
+// alert(k);
+// alert(l);
+// alert(m);
+
+
+document.write(a);
+document.write(b);
+document.write(c);
+document.write(d);
+document.write(e);
+document.write(f);
+document.write(g);
+document.write(h);
+document.write(i);
+document.write(j);
+document.write(k);
+document.write(l);
+document.write(m);
+
+
+let firstName = "Evgen ";
+let middleName = "Mikolaevich ";
+let lastName = "Evchenko";
+let age = 41;
+
+let first = 'firstName';
+let middle = 'middleName';
+let age1 = 'age';
+
+
+
+
+let person = firstName + middleName + lastName;
+
+// let person = `${firstName}${middleName}${lastName}`;
+console.log(person);
+
+
+let n = 100;
+let o = '100';
+let p = true;
+console.log(typeof n);
+console.log(typeof o);
+console.log(typeof p);
+
+// result = prompt(first,[firstName]);
+// result = prompt(middle,[middleName]);
+// result = prompt(age1,[age]);
---
+++ Java Script/leson2/JS2.js
@@ -0,0 +1,64 @@
+  // Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+  let list = [
+      "bucket", 'mop', 'adhesive_tape', 'glue', 'washing_powder', 'eggs', 'flour', 'bread', 'butter', 'milk'
+  ];
+  console.log(list);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+  let book1 = {title:'Fdgfhdfghhg', pageCount:  253, genre: 'Fiction'};
+  let book2 = {title: 'Dhfgh', pageCount: 452, genre: 'Non'};
+  let book3 = {title: 'Hkyhkyui', pageCount: 546, genre: 'Light_fiction'};
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив.
+  // Кожен автор має поля name,age
+
+
+  let book4 = {title:'Fdgfhdfghhg', pageCount:  253, genre: 'Fiction',authors:[name='Lesya_Ukrainka',age=154]};
+  let book5 = {title: 'Dhfgh', pageCount: 452, genre: 'Non',authors:[name='Taras_Shevchenko',age=546]};
+  let book6 = {title: 'Hkyhkyui', pageCount: 546, genre: 'Light_fiction',authors:[name='Ivan_Kotlyarvsky',age=-564]};
+
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password.
+  // Вивести в консоль пароль кожного користувача
+  let users = [
+    {name: 'Fhgfh', username: 'Hfdfgh', password: 'dfghgf215+6'},
+    {name: 'Llkj', username: 'Kfghfh', password: 'dfghgj7s84'},
+    {name: 'Flkjlkj', username: 'Dhjghj', password: 'dhfd_jhhg6'},
+    {name: 'Joklnl', username: 'Rhkjhkgh', password: '665463131'},
+    {name: 'Fibbkll', username: 'Lghj', password: 'jhgfhgj9f907'},
+    {name: 'Gkjhv', username: 'Oghjghjg', password: 'dfghj5+6'},
+    {name: 'Gjhgf', username: 'Rjghjfhg', password: 'fghjjjj215+6'},
+    {name: 'Ffhj', username: 'Qfghgfh', password: 'dfghgf456456465'},
+    {name: 'Fhfhd', username: 'Lfghfgh', password: 'dfgh4578'},
+    {name: 'Kfghfg', username: 'Dhggfghj', password: 'd454kkgf215+6'}
+  ];
+  // console.log(users [0].password) ;
+  // console.log(users [1].password) ;
+  // console.log(users [2].password) ;
+  // console.log(users [3].password) ;
+  // console.log(users [4].password) ;
+  // console.log(users [5].password) ;
+  // console.log(users [6].password) ;
+  // console.log(users [7].password) ;
+  // console.log(users [8].password) ;
+  // console.log(users [9].password) ;
+
+
+  console.log(users [0]['password']) ;
+  console.log(users [1]['password']) ;
+  console.log(users [2]['password']) ;
+  console.log(users [3]['password']) ;
+  console.log(users [4]['password']) ;
+  console.log(users [5]['password']) ;
+  console.log(users [6]['password']) ;
+  console.log(users [7]['password']) ;
+  console.log(users [8]['password']) ;
+  console.log(users [9]['password']) ;
+
+
+
+
